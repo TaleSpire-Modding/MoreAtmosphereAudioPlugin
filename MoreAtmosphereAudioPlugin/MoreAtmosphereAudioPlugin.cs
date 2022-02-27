466c467f0bc75826b1e487279218081c5359492a
brajm008 <ujkkju117@outlook.com>
2022-02-27 16:21:43 +1030

    Add project files.

---
+++ MoreAtmosphereAudioPlugin/MoreAtmosphereAudioPlugin.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using Bounce.Unmanaged;
+using HarmonyLib;
+using LordAshes;
+using UnityEngine;
+using UnityEngine.Networking;
+using MD5 = System.Security.Cryptography.MD5;
+
+namespace MoreAtmosphereAudioPlugin
+{
+
+    [BepInPlugin(Guid, "HolloFoxes' More Atmosphere Audio Plugin", Version)]
+    [BepInDependency(FileAccessPlugin.Guid)]
+    public class MoreAtmosphereAudioPlugin : BaseUnityPlugin
+    {
+        static MoreAtmosphereAudioPlugin _singleton;
+
+        // constants
+        public const string Guid = "org.hollofox.plugins.AtmosphereAudio";
+        internal const string Version = "1.0.0.0";
+
+        internal static Dictionary<NGuid, (string, string)> Music
+            = new Dictionary<NGuid, (string, string)>();
+
+        internal static Dictionary<NGuid, (string, string)> Ambient
+            = new Dictionary<NGuid, (string, string)>();
+
+        private void LoadAudio(string SubFolder)
+        {
+            var files =
+                FileAccessPlugin.File.Find($"CustomData\\Audio\\{SubFolder}")
+                    .Where(f =>
+                        f.EndsWith(".mp3") ||
+                        f.EndsWith(".aif") ||
+                        f.EndsWith(".wav") ||
+                        f.EndsWith(".ogg")
+                    );
+
+            foreach (var file in files)
+            {
+                string songPath = 
+                    file.StartsWith("https://") || file.StartsWith("http://") ? 
+                        file : $"file:///{file}";
+                var components = file.Split('/');
+                var builder = new StringBuilder();
+                for (int i = 0; i < 5; i++)
+                {
+                    builder.Append(components[components.Length - 1 - i]);
+                }
+                var id = GenerateID(builder.ToString());
+
+                var name = components.Last();
+                Debug.Log($"Added:{name}");
+                name = name
+                    .Replace(".mp3", "")
+                    .Replace(".aif", "")
+                    .Replace(".wav", "")
+                    .Replace(".ogg", "");
+
+                switch (SubFolder)
+                {
+                    case "Ambient":
+                        Ambient.Add(id, (name, songPath));
+                        break;
+                    case "Music":
+                        Music.Add(id, (name, songPath));
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Awake plugin
+        /// </summary>
+        void Awake()
+        {
+            Debug.Log("More Atmosphere Audio Plug-in loaded");
+            ModdingUtils.Initialize(this, Logger);
+            var harmony = new Harmony(Guid);
+            harmony.PatchAll();
+            LoadAudio("Ambient");
+            LoadAudio("Music");
+            _singleton = this;
+        }
+
+
+        public static void LoadAudioCallback(object[] args)
+        {
+            _singleton.StartCoroutine("LoadAudioFromSource", args);
+        }
+
+        public static NGuid GenerateID(string id)
+        {
+            return new NGuid(System.Guid.Parse(CreateMD5(id)));
+        }
+
+        private static string CreateMD5(string input)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var inputBytes = Encoding.ASCII.GetBytes(input);
+                var hashBytes = md5.ComputeHash(inputBytes);
+                var sb = new StringBuilder();
+                for (var i = 0; i < hashBytes.Length; i++) sb.Append(hashBytes[i].ToString("X2"));
+                return sb.ToString();
+            }
+        }
+
+        IEnumerator LoadAudioFromSource(object[] args)
+        {
+            var __instance = (AtmosphereManager.LoadedAudioClip)args[0];
+            var ____clip = (AudioClip) args[1];
+            var ClipLoaded = (System.Action<AtmosphereManager.LoadedAudioClip, AudioClip >) args[2];
+            
+            var source = Ambient.ContainsKey(__instance.GUID)
+                ? Ambient[__instance.GUID].Item2
+                : Music[__instance.GUID].Item2;
+
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(source, AudioType.UNKNOWN))
+            {
+                yield return www.SendWebRequest();
+                if (www.result == UnityWebRequest.Result.ConnectionError)
+                {
+                    Debug.Log("Audio Plugin: Failure To Load ...");
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    ____clip = DownloadHandlerAudioClip.GetContent(www);
+                }
+            }
+            ClipLoaded(__instance, ____clip);
+        }
+
+    }
+}
---
+++ MoreAtmosphereAudioPlugin/Patches/AtmosphereManagerPatch.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using Bounce.BlobAssets;
+using Bounce.TaleSpire.AssetManagement;
+using Bounce.Unmanaged;
+using HarmonyLib;
+using TMPro;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace MoreAtmosphereAudioPlugin.Patches
+{
+    [HarmonyPatch(typeof(AssetDb), "OnSetupInternals")]
+    public class AtmosphereManagerPatch
+    {
+        static void Postfix()
+        {
+            foreach (var file in MoreAtmosphereAudioPlugin.Ambient)
+            {
+                if (AssetDb.Music.TryGetValue(file.Key, out _)) continue;
+                var builder = new BlobBuilder(Allocator.Persistent);
+                builder.ConstructRoot<MusicData>();
+                var x = builder.CreateBlobAssetReference<MusicData>(Allocator.Persistent);
+                x.Value = new MusicData();
+                MusicData.Construct(builder,ref x.Value,file.Key,file.Key,file.Value.Item1,file.Value.Item1,
+                    new string[]{},"", file.Value.Item1, MusicData.MusicKind.Ambient);
+                                      
+                Debug.Log($"{MusicData.MusicKind.Ambient} Added:" + AssetDb.Music.TryAdd(file.Key, x.TakeView()));
+            }
+
+            foreach (var file in MoreAtmosphereAudioPlugin.Music)
+            {
+                if (AssetDb.Music.TryGetValue(file.Key, out _)) continue;
+                var builder = new BlobBuilder(Allocator.Persistent);
+                builder.ConstructRoot<MusicData>();
+                var x = builder.CreateBlobAssetReference<MusicData>(Allocator.Persistent);
+                x.Value = new MusicData();
+                MusicData.Construct(builder, ref x.Value, file.Key, file.Key, file.Value.Item1, file.Value.Item1,
+                    new string[] { }, "", file.Value.Item1, MusicData.MusicKind.Music);
+                Debug.Log($"{MusicData.MusicKind.Music} Added:" + AssetDb.Music.TryAdd(file.Key, x.TakeView()));
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(AtmosphereManager.LoadedAudioClip), "Load")]
+    public class LoadedAudioClipLoadPatch
+    {
+        static bool Prefix(System.Action<AtmosphereManager.LoadedAudioClip, AudioClip> ClipLoaded,
+            ref AudioClip ____clip,
+            ref AtmosphereManager.LoadedAudioClip __instance
+        )
+        {
+            if (MoreAtmosphereAudioPlugin.Ambient.ContainsKey(__instance.GUID) ||
+                MoreAtmosphereAudioPlugin.Music.ContainsKey(__instance.GUID))
+            {
+                if (____clip == null)
+                {
+                    MoreAtmosphereAudioPlugin.LoadAudioCallback(
+                        new object[]
+                        {
+                            __instance,
+                            ____clip,
+                            ClipLoaded
+                        }
+                        );
+                    return false;
+                }
+            }
+            return true;
+            
+        }
+    }
+
+    
+    [HarmonyPatch(typeof(UI_MusicSelectionDropdown), "OnEnable")]
+    public class UI_MusicSelectionDropdownPatch
+    {
+        static void Postfix(
+            ref TMP_Dropdown ____dropdown,
+            ref List<NGuid> ____guidList,
+            ref List<string> ____options,
+            ref MusicData.MusicKind ____type
+        )
+        {
+            var files = MoreAtmosphereAudioPlugin.Ambient;
+            if (____type == MusicData.MusicKind.Music)
+                files = MoreAtmosphereAudioPlugin.Music;
+            foreach (var file in files)
+            {
+                var index = ____guidList.IndexOf(file.Key);
+                if (index > -1) ____options[index+1] = file.Value.Item1;
+            }
+            ____dropdown.options.Clear();
+            ____dropdown.AddOptions(____options);
+        }
+    }
+}
